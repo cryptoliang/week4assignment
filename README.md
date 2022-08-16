b8b1ff042be8abf7c83341a1dcb9a46afc866a37
Liang Chen <liang.chen@crypto.com>
2022-08-16 17:38:14 +0800

    Add README.md change refactor change price script.

--- README.md
+++ README.md
@@ -1,12 +1,99 @@
 # Leverage Trading with VVS and Tectonic
 
+## Introduction
+
+Support leverage trading with VVS and Tectonic.
+
+## How to Demo
+
+### Prepare
+
+Send USDC to accounts
 
 ```shell
 hh run scripts/sendUSDC.ts
-hh run scripts/deploy.ts
+```
+
+Add liquidity to WETH/USDC liquidity pool
+
+```shell
 hh run scripts/addLiquidity.ts 
-hh run scripts/changeOraclePrice.ts
-hh run scripts/changeVVSPrice.ts
+```
+
+Deploy the contract
+
+```shell
+hh run scripts/deploy.ts
+```
+
+### Long
+
+Change USD/ETH price to 1000.
+
+```shell
+PRICE=1000 hh run scripts/changePrice.ts
+```
+
+Open Long position
+
+- Input: 1000 USDC
+- Loan to Value Ratio: 60%
+
+| round           | 1st | 2nd | 3rd  | total |
+|-----------------|-----|-----|------|-------|
+| USDC Borrowed   |     | 600 | 360  | 960   |
+| WETH Collateral | 1   | 0.6 | 0.36 | 1.96  |
+
+```shell
 hh run scripts/openLongPosition.ts 
+```
+
+Change USD/ETH price to 2000.
+
+```shell
+PRICE=2000 hh run scripts/changePrice.ts
+```
+
+Close long position
+
+Estimate profit: 1000 * 1.96 = 1960.
+
+```shell
 hh run scripts/closeLongPosition.ts 
+```
+
+### Short
+
+Change USD/ETH price to 1000.
+
+```shell
+PRICE=1000 hh run scripts/changePrice.ts
+```
+
+Open short position
+
+- Input: 1000 USDC
+- Loan to Value Ratio: 60%
+
+| round           | start | 1st | 2nd  | 3rd   | total |
+|-----------------|-------|-----|------|-------|-------|
+| WETH Borrowed   |       | 0.6 | 0.36 | 0.216 | 1.176 |
+| USDC Collateral | 1000  | 600 | 360  | 216   | 2176  |
+
+```shell
+hh run scripts/openShortPosition.ts 
+```
+
+Change USD/ETH price to 500.
+
+```shell
+PRICE=500 hh run scripts/changePrice.ts
+```
+
+Close short position
+
+Estimate profit: 500 * 1.176 = 588.
+
+```shell
+hh run scripts/closeShortPosition.ts 
 ```
--- scripts/changePrice.ts
+++ scripts/changePrice.ts
@@ -1,4 +1,5 @@
 import {ethers, network} from "hardhat";
+import cronosOracleABI from "../abis/cronosOracle"
 import cronosCRC20ABI from "../abis/cronosCRC20"
 import vvsRouterABI from "../abis/vvsRouter"
 import {Contract} from "ethers";
@@ -9,11 +10,37 @@ const wethAddr = "0xe44Fd7fCb2b1581822D0c862B68222998a0c299a";
 const wethOwnerAddr = "0xea01e309146551a67366fe9943E9Ed83Ae564057";
 const ethUsdcPairAddress = "0xfd0Cd0C651569D1e2e3c768AC0FFDAB3C8F4844f";
 const vvsRouterAddress = "0x145863Eb42Cf62847A6Ca784e6416C1682b1b2Ae";
-const deadline = Math.floor(Date.now() / 1000) + 180;
+const usdEthPriceOracleAddr = "0x6850A6e773b9a625C6810E34070491d0FF97E065";
+const usdEthPriceOracleOwnerAddr = "0x71F0cDb17454ad7EeB7e26242292fe0E0189645a";
 
-async function main() {
+const targetUsdcPerETH = (process.env.PRICE && parseInt(process.env.PRICE)) || 1000;
+
+async function changeOraclePrice() {
+    console.log("=========== Change USD/ETH Oracle price =============")
+
+    await network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [usdEthPriceOracleOwnerAddr],
+    });
+
+    let owner = await ethers.getImpersonatedSigner(usdEthPriceOracleOwnerAddr);
+
+    const cronosOracle = new ethers.Contract(usdEthPriceOracleAddr, cronosOracleABI, owner);
+
+    const timestamp = Math.floor(Date.now() / 1000);
+    const roundId = timestamp * 100000;
+
+    let ethPriceBefore = await cronosOracle.latestAnswer();
+    console.log("USD/ETH Oracle price before:", ethers.utils.formatUnits(ethPriceBefore, 8));
+
+    await cronosOracle.updatePrice(roundId, timestamp, targetUsdcPerETH * 10 ** 8);
+
+    let etherPriceAfter = await cronosOracle.latestAnswer();
+    console.log("USD/ETH Oracle price after:", ethers.utils.formatUnits(etherPriceAfter, 8));
+}
+
+async function changeVVSPrice() {
     console.log("=========== Change VVS WETH/USDC swap price =============")
-    const targetUsdcPerETH = 1000;
 
     await network.provider.request({method: "hardhat_impersonateAccount", params: [usdcOwnerAddr]});
     await network.provider.request({method: "hardhat_impersonateAccount", params: [wethOwnerAddr]});
@@ -44,6 +71,7 @@ async function main() {
     let usdcDesired = Number(Math.sqrt(k * targetUsdcPerETH).toFixed(6));
     let wethDesired = Math.sqrt(k / targetUsdcPerETH);
 
+    const deadline = Math.floor(Date.now() / 1000) + 180;
     if (wethDesired > wethCurrent) {
         let wethInput = ethers.utils.parseUnits((wethDesired - wethCurrent).toString(), 18);
 
@@ -74,6 +102,11 @@ async function getAndLogBalance(token: Contract, addr: string): Promise<number> 
     let symbol = await token.symbol();
     console.log(`${symbol} balance: ${amount}`)
     return amount
+}
+
+async function main() {
+    await changeOraclePrice();
+    await changeVVSPrice();
 }
 
 main().catch((error) => {
